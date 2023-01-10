a5f558f6a868ba46f1227b5a79d0587cf6265db1
Sebastien COUDERT <sebastien.coudert@ganil.fr>
2023-01-10 13:47:37 +0100

    tabs4keyb_scrin_air_F?-F*.png to tabs4keyb_scrin_air_F?.png

--- FFtoF.sh
+++ FFtoF.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
 fi=tabs4keyb_scrin_air_F1-F4.png
+fi=
 
 w=64
-for((i=1;i<5;++i))
+list=
+for((s=1,e=4;e<13;s+=4,e+=4))
 do
+  b=F$s-F$e
+  fi=tabs4keyb_scrin_air_$b.png
   fb=`basename $fi .png`
-  fo=`echo $fb | sed "s/F.-F.//"`F$i.png
-  ((x0=($w-1) *(i-1)))
-  convert -crop ${w}x48+${x0} $fi $fo
-  list=$list' '$fo
+  for((i=0;i<4;++i))
+  do
+    ((f=i+s))
+    fo=`echo $fb | sed "s/$b//"`F$f.png
+    ((x0=($w-1)*i))
+    convert -crop ${w}x48+${x0} $fi $fo
+    list=$list' '$fo
+  done
 done
 
 display $list
