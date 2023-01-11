c29f167815631873778bff4187091bebd93d4e5c
Sebastien COUDERT <sebastien.coudert@ganil.fr>
2023-01-11 13:49:56 +0100

    tree: adding all nodes (WinP)

--- FtoG.sh
+++ FtoG.sh
@@ -3,18 +3,41 @@
 fi=tabs4keyb_scrin_air_F?.png
 fi=
 
-list=
-for t in build shield walk vehicle air
+list="build shield walk vehicle air"
+for t in $list
 do
-  echo "//"$t
+  echo "  //"$t" nodes"
   i=1
   for f in tabs4keyb_scrin_${t}_F?.png
   do
-    echo $t'_F'$i'[label="F'$i'", color="white", shape="rect", image="'$f'"]'
+    echo "  "$t'_F'$i'[label="F'$i'", color="white", shape="rect", image="'$f'"]'
     ((++i))
   done
 done
+for t in $list
+do
+  echo "  ///"$t" links"
+  i=1; j=2
+  echo '  build_F_TODO -> '$t'_F'$i'[color="blue"]'
+  for f in tabs4keyb_scrin_${t}_F?.png
+  do
+    echo "              "$t'_F'$i' -> '$t'_F'$j'[color="white"]'
+    ((++i,++j))
+  done
+done
 
-#exit
 
-display $list
+
+exit
+
+//walk
+walk_F1[label="F1", color="white", shape="rect", image="tabs4keyb_scrin_walk_F1.png"]
+walk_F2[label="F2", color="white", shape="rect", image="tabs4keyb_scrin_walk_F2.png"]
+
+exit
+
+
+///walk
+  build_F2 -> walk_F1[color="blue"]
+              walk_F1-> walk_F2[color="white"]
+              
